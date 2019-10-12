d0e3c6709f9bf3eecf1f7cac5ce7d820dcd31cbb
Justin Aiello <justinaiello@gmail.com>
2019-10-12 16:52:41 -0400

    Setting up dependencies and config.

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,9 +1,60 @@
-/**
- * Configure your Gatsby site with this file.
- *
- * See: https://www.gatsbyjs.org/docs/gatsby-config/
- */
-
 module.exports = {
-  /* Your site config here */
+  siteMetadata: {
+    title: "Gatsby Site",
+  },
+  plugins: [
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `pages`,
+        path: `${__dirname}/src/`,
+      },
+    },
+    {
+      resolve: `gatsby-plugin-styled-components`,
+      options: {
+        // Add any options here
+      },
+    },
+    {
+      resolve: "gatsby-transformer-remark",
+      options: {
+        commonmark: true,
+        footnotes: true,
+        pedantic: true,
+        gfm: true,
+        plugins: [
+          {
+            resolve: "gatsby-remark-embed-video",
+            options: {
+              width: 800,
+              ratio: 1.77,
+              height: 400,
+              related: false,
+              noIframeBorder: true,
+              urlOverrides: [
+                {
+                  id: "youtube",
+                  embedURL: videoId =>
+                    `https://www.youtube-nocookie.com/embed/${videoId}`,
+                },
+              ], //Optional: Override URL of a service provider, e.g to enable youtube-nocookie support
+            },
+          },
+        ],
+      },
+    },
+    {
+      resolve: "gatsby-plugin-sharp",
+    },
+    {
+      resolve: "gatsby-transformer-sharp",
+    },
+    {
+      resolve: `gatsby-source-instagram`,
+      options: {
+        username: `puppystagrams`,
+      },
+    },
+  ],
 }
