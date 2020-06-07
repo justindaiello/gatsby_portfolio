85dea2efebc751b50db202ae31201b09f332b906
Justin Aiello <justinaiello@gmail.com>
2020-06-07 13:42:39 -0400

    feat(hero): add selfie

--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -1,13 +1,45 @@
 import React from 'react';
 import { useSelector } from 'react-redux';
+import { useStaticQuery, graphql } from 'gatsby';
 
-import { StyledHero, StyledList } from './HeroStyles';
+import { HeroContainer, StyledHero, StyledList, StyledImg } from './HeroStyles';
 
 function Hero() {
   const { isHidden } = useSelector((state) => state);
+
+  const data = useStaticQuery(graphql`
+    query Image {
+      selfie: file(relativePath: { eq: "Justin4x.png" }) {
+        id
+        childImageSharp {
+          fixed(pngQuality: 100, height: 200) {
+            ...GatsbyImageSharpFixed
+          }
+        }
+      }
+      selfieMobile: file(relativePath: { eq: "Justin4x.png" }) {
+        id
+        childImageSharp {
+          fixed(pngQuality: 100, height: 125) {
+            ...GatsbyImageSharpFixed
+          }
+        }
+      }
+    }
+  `);
+
+  const selfieSources = [
+    data.selfie.childImageSharp.fixed,
+    {
+      ...data.selfieMobile.childImageSharp.fixed,
+      media: '(max-width: 600px)',
+    },
+  ];
+
   return (
-    <div style={{ height: '100vh' }}>
+    <HeroContainer>
       <StyledHero>
+        <StyledImg isHidden={isHidden} fixed={selfieSources} />
         <h1>Justin Aiello</h1>
         {!isHidden && (
           <>
@@ -48,7 +80,7 @@ function Hero() {
           </>
         )}
       </StyledHero>
-    </div>
+    </HeroContainer>
   );
 }
 
