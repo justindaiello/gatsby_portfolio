de53ccec786a9f32d906d8b64f3209dfe092ef04
Justin Aiello <justinaiello@gmail.com>
2020-06-06 22:05:51 -0400

    contrib(linus_page): add image box-shadows

--- src/components/LinusContent/LinusStyles.js
+++ src/components/LinusContent/LinusStyles.js
@@ -158,6 +158,37 @@ const StyledImgContainer = styled.div`
 
   .appImage {
     border-radius: 12px;
+    box-shadow: ${(props) => props.theme.boxShadow};
+  }
+
+  @media (max-width: 999px) {
+    grid-template-columns: repeat(2, 1fr);
+
+    .appImage {
+      justify-self: center;
+      width: 375px;
+    }
+
+    .appImage:last-child,
+    .appImage:first-child {
+      grid-row: 1;
+    }
+
+    .appImage:nth-of-type(2) {
+      grid-column: 1 / 3;
+    }
+  }
+
+  @media (max-width: 850px) {
+    grid-template-columns: 1fr;
+
+    .appImage:first-child {
+      grid-row: 1;
+    }
+
+    .appImage:last-child {
+      grid-row: 3;
+    }
   }
 `;
 
