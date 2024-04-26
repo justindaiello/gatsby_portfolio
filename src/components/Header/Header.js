2c731dd734153f42c9d434b8c0f38f84e0e1e8e9
Justin Aiello <justinaiello@gmail.com>
2024-04-26 15:37:06 -0400

    chore: remove some ui for now and update link

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,12 +1,11 @@
 import React from 'react';
-import { bool, func, object } from 'prop-types';
 import { useDispatch } from 'react-redux';
 
 import actions from '../../context/actions';
 import { StyledHeader, StyledButton } from './HeaderStyles';
-import { handleScrollDown } from '../../utils/functions';
+/* TODO: use this for new stuff import { handleScrollDown } from '../../utils/functions'; */
 
-const Header = ({ projectRef }) => {
+const Header = () => {
   const dispatch = useDispatch();
 
   function handleAboutClick() {
@@ -17,17 +16,8 @@ const Header = ({ projectRef }) => {
   return (
     <StyledHeader>
       <StyledButton onClick={handleAboutClick}>About</StyledButton>
-      <StyledButton onClick={() => handleScrollDown(projectRef)}>
-        Work
-      </StyledButton>
     </StyledHeader>
   );
-};
-
-Header.propTypes = {
-  hidden: bool,
-  setIsHidden: func,
-  projectRef: object,
 };
 
 export default Header;
--- src/pages/index.js
+++ src/pages/index.js
@@ -3,7 +3,6 @@ import React from 'react';
 import Hero from '../components/Hero';
 import SEO from '../components/SEO/SEO';
 import Header from '../components/Header';
-import Projects from '../components/Projects';
 import TransitionSpring from '../components/TransitionSpring';
 
 function IndexPage() {
@@ -14,7 +13,6 @@ function IndexPage() {
       <SEO title="Asheville, NC" />
       <Header projectRef={projectRef} />
       <Hero />
-      <Projects projectRef={projectRef} />
     </TransitionSpring>
   );
 }
